348bc8721ee08e37855bb615f7ab861f0ba3cf68
ilya <ilyadvilyanski@gmail.com>
2023-03-02 17:04:20 -0500

    rearranging layout + bootstrap css

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getBooks(event) {
     let query = document.getElementsByName('bookcategory')[0].value;
-    fetch('http://openlibrary.org/subjects/' + query + '.json?details=false&limit=120').then(response => {
+    fetch('http://openlibrary.org/subjects/' + query + '.json?details=false&limit=10').then(response => {
             return response.json();
         })
         .then(data => {
@@ -9,7 +9,9 @@ function getBooks(event) {
             if (data.works.length === 0) {
                 writeDefault();
             } else {
-                data.works.filter(book => stringIsEnglish(book.title)).forEach((value) => writeToDoc(value));
+                data.works.filter(book => stringIsEnglish(book.title)).forEach((value) => {
+                    writeToDoc(value)
+                });
             }
         })
         .catch(err => {
@@ -18,17 +20,21 @@ function getBooks(event) {
 }
 
 function writeToDoc(book) {
-    document.getElementById('content').innerHTML += '<a target="_blank" href="https://openlibrary.org' + book.key + '">' + book.title + '</a> by ' + book.authors[0].name + '<br>';
+    if(!book.availability.isbn) {
+        console.log("didn't have isbn: " + book.title)
+    }
+    document.getElementById('content').innerHTML 
+    += '<div class="col-lg"><img class="img-fluid w-100 mb-3 img-thumbnail shadow-sm rounded-0" src="https://covers.openlibrary.org/b/isbn/' + book.availability.isbn + '.jpg"><h2 class="h4">'+ book.authors[0].name +'</h2></div>';
 }
 
 function writeDefault() {
     document.getElementById('content').innerHTML = 'No Results for Query'
 }
 
-var ENGLISH = " '-,abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const ENGLISH = " '-,abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 function stringIsEnglish(str) {
-    var index;
+    let index;
 
     for (index = str.length - 1; index >= 0; --index) {
         if (ENGLISH.indexOf(str.substring(index, index + 1)) < 0) {
