d8e3d337648d50f49ccb39520bbd5945ff2499d2
ilya <ilyadvilyanski@gmail.com>
2023-03-06 11:20:24 -0500

    style updates and js logic cleanup

--- js/index.js
+++ js/index.js
@@ -1,45 +1,79 @@
-function getBooks(event) {
-    let query = document.getElementsByName('bookcategory')[0].value;
-    fetch('http://openlibrary.org/subjects/' + query + '.json?details=false&limit=10').then(response => {
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById('content').innerHTML = "";
+let searchTxt = document.getElementById('searchbox');
+let searchBtn = document.getElementById('searchbtn');
 
-            if (data.works.length === 0) {
-                writeDefault();
-            } else {
-                data.works.filter(book => stringIsEnglish(book.title)).forEach((value) => {
-                    writeToDoc(value)
-                });
-            }
-        })
+// press enter to search
+searchTxt.addEventListener('keypress', function (e) {
+    if (e.key == 'Enter') {
+        e.preventDefault()
+
+        searchBtn.click();
+        searchBtn.disabled = true
+        setTimeout(function () {
+            searchBtn.disabled = false
+        }, 1000)
+
+    }
+})
+
+// grabs books from OpenLib
+function getBooks(event) {
+    fetch(`http://openlibrary.org/subjects/${document.getElementsByName('bookcategory')[0].value}.json?details=false&limit=10`).then(response => response.json())
+    .then(data => {
+        if (data.works.length === 0) {
+            writeDefault();
+        } else {
+
+        document.getElementById('content').innerHTML = "";
+
+        data.works.forEach((book) => {
+    fetch(`http://openlibrary.org${book.key}.json`)
+    .then(response => response.json()).then(bookInfo => {
+                let bookCollectedInfo = {}
+
+                if (!book.availability) {
+                    console.log("didn't have isbn: " + book.title)
+                } else {
+                    bookCollectedInfo.isbn = book.availability.isbn
+                    bookCollectedInfo.title = book.title
+                    bookCollectedInfo.authorsKey = book.authors[0].key
+                    bookCollectedInfo.authorsName = book.authors[0].name
+                    bookCollectedInfo.desc = bookInfo.description.value
+
+                    writeToDoc(bookCollectedInfo)
+                }
+            })
+        });
+        }
+    })
         .catch(err => {
             console.log(err)
         });
 }
 
+// places results into html
 function writeToDoc(book) {
-    if(!book.availability.isbn) {
-        console.log("didn't have isbn: " + book.title)
-    }
-    document.getElementById('content').innerHTML 
-    += '<div class="col-lg"><img class="img-fluid w-100 mb-3 img-thumbnail shadow-sm rounded-0" src="https://covers.openlibrary.org/b/isbn/' + book.availability.isbn + '.jpg"><h2 class="h4">'+ book.authors[0].name +'</h2></div>';
+
+    console.log(book)
+
+    document.getElementById('content').innerHTML
+        += `<div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" src="https://covers.openlibrary.org/b/isbn/${book.isbn}.jpg" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">${book.desc}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+              <a target="new" href="https://openlibrary.org${book.authorsKey}/${book.authorsName.replace(' ', '_')}"><button type="button" class="btn btn-sm btn-outline-secondary">${book.authorsName}</button></a>
+                <button type="button" class="btn btn-sm btn-outline-secondary">book</button>
+              </div>
+              <small class="text-muted">9 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>`
 }
 
+// default text of no search results
 function writeDefault() {
     document.getElementById('content').innerHTML = 'No Results for Query'
-}
-
-const ENGLISH = " '-,abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-function stringIsEnglish(str) {
-    let index;
-
-    for (index = str.length - 1; index >= 0; --index) {
-        if (ENGLISH.indexOf(str.substring(index, index + 1)) < 0) {
-            return false;
-        }
-    }
-    return true;
 }
